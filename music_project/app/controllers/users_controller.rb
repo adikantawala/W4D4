ec84835003f9fa76428a02194a5df38fb8a735fe
Adi Kantawala <adikan@ucla.edu>
2018-11-15 16:36:19 -0800

    Finish warm up

---
+++ music_project/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_url(user)
+    else
+      render json: "bad"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
